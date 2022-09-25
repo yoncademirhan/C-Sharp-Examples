afc99cd17b63e5932daacc794a53400bc6427b00
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-09-25 11:58:55 +0300

    Add files via upload

---
+++ 8- Hazir_String_Metotlar_Concat/Hazir_String_Metotlar_Concat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazir_String_Metotlar_Concat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Concat: Parantez içinde yazılan verileri/ değişkenleri string veri türünde birleştirme işlemi yapar. Birleştiirlecek veriler string olmak zorunde değildir.
+
+            string tcNo = "12458545788";
+            string ad = "Ahmet";
+            string soyAd = "Kaya";
+            double maas = 3000;
+            int cocukSayisi = 4;
+
+            string birlestirilenVeri= string.Concat(tcNo + " " +  ad + " " + soyAd + " " + maas + " " + cocukSayisi);
+            Console.WriteLine(birlestirilenVeri);
+
+            Console.ReadKey();
+
+        }
+    }
+}
