22e235bfba67c8358102777e7e1fc4e115b6f482
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-01 12:44:44 +0300

    Add files via upload

---
+++ 12- Metot_Overload/Metot_Overload/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot_Overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Topla(4.5, 8.1);
+
+
+            Console.ReadKey();
+        }
+
+        static void Topla(int s1, int s2)
+        {
+            Console.WriteLine("Toplam: " + (s1+ s2));
+        }
+
+        static void Topla(double s1, double s2)
+        {
+            Console.WriteLine("Toplam: " + (s1 + s2));
+        }
+    }
+}
