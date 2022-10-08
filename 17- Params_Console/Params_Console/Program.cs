53aa1076b9934197c7d4241ff257020cde2e1696
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-08 10:38:04 +0300

    Add files via upload

---
+++ 17- Params_Console/Params_Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(topla(2, 3, 5, 4, 8, 6, 7));
+
+            Console.ReadLine();
+
+           // return 0;
+            
+        }
+
+        static int topla(params int[] sayilar)
+        {
+            int toplam = 0;
+
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+            }
+
+            return toplam;
+        }
+    }
+}
