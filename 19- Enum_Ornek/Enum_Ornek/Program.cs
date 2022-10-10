fac906c0cddf92230187887ff455a9f6853f0804
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-10-10 23:17:02 +0300

    Add files via upload

---
+++ 19- Enum_Ornek/Enum_Ornek/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Ornek
+{
+    internal class Program
+    {
+
+        enum gunler
+        {
+            Pazartesi= 1, Sali, Carsamba, Persembe, Cuma, Cumartesi, Pazar              //eğer pazartesi 1 değeri verilmez ise enumlarda ilk değer sıfır(0) kabul edilir. sanraki değerler 1 er artarak devam eder.
+        }
+        static void Main(string[] args)
+        {
+            int sayi = (int)gunler.Persembe;
+            Console.WriteLine(sayi + ".gündür");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
